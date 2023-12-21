295984b324935ee82ecd869e3f18843c383a09a6
Minh <beaver700nh@gmail.com>
2023-12-21 17:04:14 -0500

    Added Javadocs

--- src/main/java/frc/robot/subsystems/PneumaticsSubsystem.java
+++ src/main/java/frc/robot/subsystems/PneumaticsSubsystem.java
@@ -14,16 +14,27 @@ public class PneumaticsSubsystem extends SubsystemBase {
     m_solenoid.set(false);
   }
 
+  /**
+   * Opens valve to release pressure into barrel.
+   */
   private void launch() {
     System.out.println("Launching!");
     m_solenoid.set(true);
   }
 
+  /**
+   * Closes barrel pressure valve.
+   */
   private void stopLaunch() {
     m_solenoid.set(false);
     System.out.println("... launch stopped.");
   }
 
+  /**
+   * Generate a command that launches the ball for 1/8 second.
+   *
+   * @return The launch command.
+   */
   public CommandBase launchCommand() {
     return new SequentialCommandGroup(
       runOnce(this::launch),
